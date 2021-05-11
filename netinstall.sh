a8c9374a0b5abff0da8969bc2de25b94ef9669ce
Fikret Musk <54106274+fikret0@users.noreply.github.com>
2021-05-11 09:00:10 +0300

    Update netinstall.sh

--- netinstall.sh
+++ netinstall.sh
@@ -5,16 +5,15 @@ fi
 
 echo ":: Downloading..."
 
-[ -f "/tmp/sjfetch.zip" ] && rm -rf "/tmp/sjfetch.zip"
+[ -d "/tmp/sjfetch-git" ] && rm -rf "/tmp/sjfetch-git"
 [ -d "/usr/share/sjfetch" ] && rm -rf "/usr/share/sjfetch"
 [ -L "/usr/bin/sjfetch" ] && rm -rf "/usr/bin/sjfetch"
 
-wget "https://github.com/fikret0/sjfetch/releases/download/1.0/sjfetch.zip" -O "/tmp/sjfetch.zip"
+git clone "https://github.com/fikret0/sjfetch.git" "/tmp/sjfetch-git"
 mkdir "/usr/share/sjfetch"
 
 echo ":: Installing..."
 
-unzip "/tmp/sjfetch.zip" -d "/usr/share/sjfetch"
-ln -s "/usr/share/sjfetch/sjfetch" "/usr/bin/sjfetch"
+/tmp/sjfetch-git/install.sh
 
 echo ":: Done."
